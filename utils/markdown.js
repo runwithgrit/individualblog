5a95f784900540e69c9ce608cdd7fc32242fd811
yunyuyuan <yun-yu-yuan@qq.com>
2022-04-17 00:27:26 +0800

    fix:markdownparser

--- utils/markdown.js
+++ utils/markdown.js
@@ -6,16 +6,28 @@ import ClipboardJS from "clipboard";
 import { notify } from "./utils";
 import hljs from 'highlight.js';
 
+/* FIXME hack: recursive regex replace. because fucking safari don't suppot lookbehind, all of `(^|(?>=[^\\]))` need to be changed to `(^|[^\\])` */
+function recursiveReplace(text, regex, cb) {
+  let result = text;
+  while (regex.test(result)) {
+    result = result.replace(new RegExp(regex, 'g'), cb);
+  }
+  return result;
+}
+
 const
   paragraphTabExtension = {
     type: 'lang',
-    regex: '(^|(?<=[^\\\\]))<<>>',
-    replace: '$1&emsp;&emsp;'
+    filter(text, converter) {
+      return recursiveReplace(text, /(^|[^\\])<<>>/, (a, prefix) => {
+        return `${prefix}&emsp;&emsp;`
+      });
+    }
   },
   headerExtension = {
     type: 'lang',
     filter(text, converter) {
-      return text.replace(/(^|\n)(#{1,6})\s*(.+?)\s*(?=\n|$)/g, (a, prefix, tone, content) => {
+      return recursiveReplace(text, /(^|\n)(#{1,6})\s*(.+?)\s*(?=\n|$)/, (a, prefix, tone, content) => {
         const size = tone.length;
         const anchor = 'id-' + encodeURIComponent(content);
         return `${prefix}<h${size}><sup class="fake-head" id="${anchor}"></sup><a class="header-link" href="#${anchor}">${converter.makeHtml(content)}</a></h${size}>`
@@ -24,61 +36,80 @@ const
   },
   blankLinkExtension = {
     type: 'lang',
-    regex: '(^|[^\\\\])#\\[(.*?)]\\((.*?)\\)',
-    replace: '$1<a target="_blank" href="$3">$2</a>'
+    filter(text, converter) {
+      return recursiveReplace(text, /(^|\n)(#{1,6})\s*(.+?)\s*(?=\n|$)/, (a, prefix, txt, link) => {
+        return `${prefix}<a target="_blank" href="${link}">${txt}</a>`
+      })
+    }
   },
   commonImgExtension = {
     type: 'lang',
-    regex: '(^|(?<=[^\\\\]))!\\[(.*?)]\\((.*?)\\)',
-    replace: (a, prefix, alt, src) => {
-      // sticker
-      if (alt === 'sticker') {
-        const [_, name, tone] = src.match(/^(.*?)\/(\d*)$/);
-        return `${prefix}<img src="/sticker/${name}/${tone}.png?ran=${timeStamp}" alt="${alt}"/>`
-      }
-      const mather = alt.match(/^(.*?)\[(.*?) x (.*?)]$/);
-      if (!mather) {
-        return `${prefix}<span class="image-container"><img ${viewerAttr} alt="${alt}" title="${alt}" src="${src}"/><small class="desc">${alt}</small></span>`
-      }
-      // with dimension
-      const [_, alt_, w, h] = mather;
-      let justHeight = !w;
-      return `${prefix}<span class="image-container${justHeight ? ' just-height' : ''}"><img ${viewerAttr} alt="${alt_}" title="${alt_}" style="${w ? `width: ${w} !important;` : ''}${h ? `height: ${h} !important;` : ''}" src="${src}"/><small class="desc">${alt_}</small></span>`
+    filter(text, converter) {
+      return recursiveReplace(text, /(^|[^\\])!\[(.*?)]\((.*?)\)/, (a, prefix, alt, src) => {
+        // sticker
+        if (alt === 'sticker') {
+          const matcher = src.match(/^(.*?)\/(\d*)$/);
+          if (matcher) {
+            const [_, name, tone] = matcher;
+            return `${prefix}<img src="/sticker/${name}/${tone}.png?ran=${timeStamp}" alt="${alt}"/>`
+          }
+        }
+        const mather = alt.match(/^(.*?)\[(.*?) x (.*?)]$/);
+        if (!mather) {
+          return `${prefix}<span class="image-container"><img ${viewerAttr} alt="${alt}" title="${alt}" src="${src}"/><small class="desc">${alt}</small></span>`
+        }
+        // with dimension
+        const [_, alt_, w, h] = mather;
+        let justHeight = !w;
+        return `${prefix}<span class="image-container${justHeight ? ' just-height' : ''}"><img ${viewerAttr} alt="${alt_}" title="${alt_}" style="${w ? `width: ${w} !important;` : ''}${h ? `height: ${h} !important;` : ''}" src="${src}"/><small class="desc">${alt_}</small></span>`
+      })
     }
   },
   colorTextExtension = {
     type: 'lang',
-    regex: '(^|(?<=[^\\\\]))-\\(([#a-zA-Z0-9]+): (.*?)\\)-',
-    replace: `$1<span style="color: $2">$3</span>`
+    filter(text, converter) {
+      return recursiveReplace(text, /(^|[^\\])-\(([#a-zA-Z0-9]+): (.*?)\)-/, (a, prefix, color, txt) => {
+        return `${prefix}<span style="color: ${color}">${txt}</span>`
+      })
+    }
   },
   htmlExtension = {
     type: 'lang',
-    regex: '(^|(?<=[^\\\\]))\\[html]([\\s\\S]*?)\\[\\/html]',
-    replace: `$1<span class="raw-html">$2</span>`
+    filter(text, converter) {
+      return recursiveReplace(text, /(^|[^\\])\[html]([\s\S]*?)\[\/html]/, (a, prefix, txt) => {
+        return `${prefix}<span class="raw-html">${txt}</span>`
+      })
+    }
   },
   youtubeExtension = {
     type: 'lang',
-    regex: '(^|(?<=[^\\\\]))\\[youtube]\\[(.+?)]\\((https?:\\/\\/.*?)\\)\\[\\/youtube]',
-    replace: `$1<div class="embed-video youtube">
-                        <iframe src="$3" title="$2" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <small class="desc">$2</small>
+    filter(text, converter) {
+      return recursiveReplace(text, /(^|[^\\])\[youtube]\[(.+?)]\((https?:\/\/.*?)\)\[\/youtube]/, (a, prefix, txt, src) => {
+        return `${prefix}<div class="embed-video youtube">
+                        <iframe src="${src}" title="${txt}" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <small class="desc">${txt}</small>
                     </div>`
+      })
+    }
   },
   biliExtension = {
     type: 'lang',
-    regex: '(^|(?<=[^\\\\]))\\[bili]\\[(.+?)]\\((https?:\\/\\/.*?)\\)\\[\\/bili]',
-    replace: `$1<div class="embed-video bili">
-                        <iframe src="$3" title="$2" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <small class="desc">$2</small>
+    filter(text, converter) {
+      return recursiveReplace(text, /(^|[^\\])\[bili]\[(.+?)]\((https?:\/\/.*?)\)\[\/bili]/, (a, prefix, txt, src) => {
+        return `${prefix}<div class="embed-video bili">
+                        <iframe src="${src}" title="${txt}" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <small class="desc">${txt}</small>
                     </div>`
+      })
+    }
   },
   fieldExtension = {
     type: 'lang',
     filter(text, converter) {
-      return text.replace(/(^|\n)--(.*?)--\n([\s\S]+)\n-- --/g, (a, b, c, d) => {
-        return `${b}<fieldset><legend>${converter.makeHtml(c)}</legend>${converter.makeHtml(d)}</fieldset>`
+      return recursiveReplace(text, /(^|\n)--(.*?)--\n([\s\S]+)\n-- --/, (a, prefix, legend, content) => {
+        return `${prefix}<fieldset><legend>${converter.makeHtml(legend)}</legend>${converter.makeHtml(content)}</fieldset>`
       })
-    },
+    }
   }
 
 const options = {
