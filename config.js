0bcba4e0884c284abd7a91420b7b3aa83fcb5568
runwithgrit <104905955+runwithgrit@users.noreply.github.com>
2022-05-04 20:25:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: 'starwatcher', // 网站标题
+  nickName: "观星者", // 昵称
+  githubName: 'runwithgrit', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '记录我的生与死、爱与恨',
+  SEO_keywords: 'runwithgrit,runwithgrit\'s blog,runwithgrit,观星者的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
